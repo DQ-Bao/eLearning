47efed5d974089eab31b48f6d23b9d417a93ce0a
DQ-Bao <doanqbao2506@gmail.com>
2024-05-16 20:03:08 +0700

    setup run.bat, README.md

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # eLearning
-An online learning platform
+A university web project: Create an online learning platform similar to Coursera.
+## Feature
+- Students can view and enroll courses.
+- Teachers can create and manage courses.
+## Tech
+- Java Servlet, Jsp
+- Tomcat 10.1
+- Apache Maven 3.9.6
+- Microsoft SQL Server 2022
+- HTML, CSS, Javascript
+## How to build and run
+### For Windows:
+- Install and configure Tomcat 10.1, Apache Maven 3.9.6 in your local machine.
+- Add the %CATALINA_HOME%, %JDK_HOME%, %JRE_HOME% to your environment variables, and the %MAVEN_HOME%/bin to your environment path variables.
+- Run the `run.bat` file in the project directory.
--- run.bat
+++ run.bat
@@ -1,24 +1,51 @@
 @echo off
 cls
 
-echo Checking for target directory...
-if not exist ".\elearning\target\" (
-    echo Target directory not found. Building the project...
+if "%CATALINA_HOME%"=="" (
+    echo CATALINA_HOME is not properly set
+    pause
+    exit
+)
+
+if "%JAVA_HOME%"=="" (
+    echo JAVA_HOME is not properly set
+    pause
+    exit
+)
+
+if "%JRE_HOME%"=="" (
+    echo JRE_HOME is not properly set
+    pause
+    exit
+)
+
+echo Checking for target war...
+
+set TARGET=.\elearning\target\elearning.war
+
+if not exist %TARGET% (
+    echo Target war not found. Building the project...
     pushd elearning
     call mvn clean install
     popd
     echo Build completed.
 ) else (
-    echo Target directory exists. Skipping build.
+    echo Target war exists. Skipping build.
 )
 
 echo Starting Tomcat...
-call startup.bat
+
+:: Edit this if using different port
+set TOMCAT_PORT=8080
+
+copy /y %TARGET% "%CATALINA_HOME%\webapps"
+
+call "%CATALINA_HOME%\bin\startup.bat"
 if %ERRORLEVEL% neq 0 (
-    echo Tomcat failed to start. Check your Tomcat10 command.
+    echo Tomcat failed to start.
     pause
     exit /b %ERRORLEVEL%
 )
 echo Tomcat started.
 
-start http://localhost:8080/elearning/
+start http://localhost:%TOMCAT_PORT%/elearning/
