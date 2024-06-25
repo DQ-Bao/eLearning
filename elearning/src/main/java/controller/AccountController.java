16bfb52229a1a7080e3e312a02be9eb12257cfe9
cuongthinhvu12@gmail.com <cuongthinhvu12@gmail.com>
2024-06-26 02:10:54 +0700

    Update user profile & Update account settings

--- elearning/src/main/java/controller/AccountController.java
+++ elearning/src/main/java/controller/AccountController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.sql.SQLException;
 import java.util.Base64;
 import data_access.UserDataAccess;
 import jakarta.servlet.ServletException;
@@ -22,17 +23,25 @@ public class AccountController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String Id = req.getParameter("id");
         String email = req.getParameter("email");
         String rawPasword = req.getParameter("oldPassword");
         String newPassword = req.getParameter("newPassword");
         String cfPassword = req.getParameter("cfPassword");
+
+        int id = Integer.parseInt(Id);
         
         if(userDAO.authenticate(email, rawPasword)!=null){
             if(newPassword == cfPassword){
-                if(userDAO.updatePassword(email, newPassword)){
-                    req.setAttribute("message", "update password successfully");
-                }else{
-                    req.setAttribute("message", "update failed");
+                try {
+                    if(userDAO.updatePassword(id, newPassword)&&userDAO.updateEmail(id, email)){
+                        req.setAttribute("message", "update account successfully");
+                    }else{
+                        req.setAttribute("message", "update failed");
+                    }
+                } catch (SQLException e) {
+                    
+                    e.printStackTrace();
                 }
             }
         }
