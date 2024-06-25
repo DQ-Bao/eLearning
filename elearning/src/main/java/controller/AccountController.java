3c9a30deabc54cd5dd8deb5c5b03eeb54f12b91a
cuongthinhvu12@gmail.com <cuongthinhvu12@gmail.com>
2024-06-26 03:00:00 +0700

    I made a mistake

--- elearning/src/main/java/controller/AccountController.java
+++ elearning/src/main/java/controller/AccountController.java
@@ -23,6 +23,11 @@ public class AccountController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String Id = req.getParameter("id");
         String email = req.getParameter("email");
         String rawPasword = req.getParameter("oldPassword");
